29445ffbff80b2e984d1bb56b7601d3cec276614
Henok100 <henokgashaw15@gmail.com>
2021-06-23 03:59:58 -0700

    edited

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int , int , int);
-int convert_day(int , int);
-void print_remaining_days(int , int, int);
+int largest_number(int x, int y, int z);
+int convert_day(int x, int y);
+void print_remaining_days(int c, int y, int r);
 
 #endif /* HOLBERTON_H */
