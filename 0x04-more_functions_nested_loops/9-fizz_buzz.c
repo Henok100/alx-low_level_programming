8659072f7486ab1723796eb6c2685f9f1eb2a291
Henok100 <henokgashaw15@gmail.com>
2021-06-24 04:57:41 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the numbers from 1 to 100
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int h;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char fibu[] = "FizzBuzz";
+
+	for (h = 1; h <= 100; h++)
+	{
+		if (h % 3 == 0 && h % 5 != 0)
+			printf("%s ", fizz);
+		else if (h % 5 == 0 && h % 3 != 0)
+			printf("%s ", buzz);
+		else if (h % 3 == 0 && h % 5 == 0)
+			printf("%s ", fibu);
+		else
+			printf("%d ", h);
+	}
+	printf("\n");
+	return (0);
+}
