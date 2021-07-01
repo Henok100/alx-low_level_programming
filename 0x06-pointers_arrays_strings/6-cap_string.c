0845e2e2e0afa26917bd681a660302609292c568
Henok100 <henokgashaw15@gmail.com>
2021-06-30 22:34:48 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @x: pointer.
+ * Return:  a pointer to the destination string x.
+ */
+
+char *cap_string(char *x)
+{
+	int i;
+
+	for (i = 0; x[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (x[i] >= 'a' && x[i] <= 'z')
+			{
+				x[i] = x[i] - 32;
+			}
+			continue;
+		}
+		if (x[i] == ' ' || x[i] == '\t' || x[i] == '.')
+		{
+			++i;
+			if (x[i] == ' ' || x[i] == '\n')
+			{
+				++i;
+			}
+			if (x[i] >= 'a' && x[i] <= 'z')
+			{
+				x[i] = x[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (x[i] >= 'A' && x[i] <= 'Z')
+			{
+				x[i] = x[i] + 32;
+			}
+		}
+	}
+}
