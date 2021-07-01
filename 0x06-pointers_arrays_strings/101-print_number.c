7e03cabc8b82b48c12e65ea07060f53fd91bc026
Henok100 <henokgashaw15@gmail.com>
2021-07-01 01:55:20 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,102 +1,23 @@
-#include "holberton.h"
+#include <stdio.h>
 
-/**
- *powB - raises the number base to power power
- *@base : the base
- *@power : the power
- *Return: return the answer
- */
-int powB(unsigned int base, int power)
+int main(void)
 {
-	int i, prod = 1;
+  int n;
+  int a[5];
+  int *p;
 
-	for (i = 0; i < power; i++)
-	{
-		prod = prod * base;
-	}
-	return (prod);
-}
-
-/**
- *numLength - returns the lenth of string
- *@num : operand number
- *Return: number of digits
- */
-int numLength(unsigned int num)
-{
-	int length = 0;
-
-	if (!num)
-		return (1);
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-
-/**
- *printnchar - Print a number of any digit
- *@n : takes an input number
- *
- *
- */
-void printnchar(int n)
-{
-	int length, j, k, digit1, tmp2;
-	unsigned int num;
-
-	if (n < 0)
-	{
-		n *= -1;
-		_putchar('-');
-	}
-	num = n;
-	length = numLength(num), j = length - 1, k, digit1, tmp2;
-	if (num == 0)
-		_putchar(48);
-	else
-	{
-		while (j >= 0)
-		{
-			if (num % powB(10, j) == 0 && j != 0)
-			{
-				_putchar(48 + num / powB(10, j));
-				for (k = j; k > 0; k--)
-					_putchar(48);
-				j = -1;
-			}
-			else
-			{
-				digit1 = num / powB(10, j);
-				_putchar(digit1 + 48);
-
-				tmp2 = num;
-				num -= powB(10, j) * digit1;
-				if (numLength(tmp2) - numLength(num) == 2)
-				{
-					_putchar(48);
-					j--;
-				}
-				j--;
-			}
-		}
-	}
-}
-
-
-
-
-/**
- * print_number - prints out any integer with only _putchar
- * @n: integer to be printed
- * Return: 0
- */
-void print_number(int n)
-{
-
-	printnchar(n);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
