a22fe36c552aacd7fa67414f6cdf10a7480e9748
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-15 21:08:09 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,62 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
+
+
 /**
- * string_nconcat - concatenates two strings
- * @s1: pointer to first string.
- * @s2: pointer to 2nd string.
- * @n: Number of bytes from n2 to concatenate.
+ * _strlen - return length of string
  *
- * Return:Pointer to space in memory containing concatenated string.
+ * @s: string to count
+ *
+ * Return: the size
  */
+
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
+
+
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes
+ * Return: pointer to new space in memory that contains s1 + n bytes of s2
+ */
+
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int i, j, s1_length, s2_length;
+	unsigned int l1, l2, i, j;
+	char *p;
 
-/*Check if the strings passed are null*/
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-/*Compute the length of the strings*/
+	l1 = s1 != NULL ? _strlen(s1) : 0;
+	l2 = s2 != NULL ? _strlen(s2) : 0;
 
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-		;
+	if (n >= l2)
+		n = l2;
+	p = malloc((l1 + n) * sizeof(char) + 1);
 
-	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-		;
-/*Memory reservation-for case 1 & 2.*/
-	str = malloc(s1_length + n + 1);
-	if (str == NULL)
-	{
+	if (p == NULL)
 		return (NULL);
-	}
-/*Copy first string into str.*/
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
-/*copy second stringg into str.*/
-	for (j = 0; j < n; j++)
+
+	for (i = 0; i < l1; i++)
 	{
-		str[i] = s2[j];
-		i++;
+		*(p + i) = s1[i];
 	}
 
-	str[i] = '\0';
-	return (str);
+	for (j = 0; j < n; j++, i++)
+		p[i] = s2[j];
+
+	p[i] = 0;
+	return (p);
 }
