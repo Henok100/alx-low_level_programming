6ea84519e137914d77946fc6bc66340d2168d88e
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-15 10:44:57 +0300

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes to be malloced
+ * Return: returns a void pointer
+ */
+void *malloc_checked(unsigned int c)
+{
+	void *p;
+
+	p = malloc(c);
+	if (!p)
+		exit(98);
+	return (p);
+}
