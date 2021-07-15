95fd58906b6c559757684e7dba4800b114554fbe
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-15 18:17:12 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: number of bytes to be malloced
- * Return: returns a void pointer
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
  */
-void *malloc_checked(unsigned int c)
-{
-	void *p;
 
-	p = malloc(c);
-	if (p == NULL)
-		exit(98);
-	return (p);
+void *malloc_checked(unsigned int b)
+{
+int *m = malloc(b);
+if (m == 0)
+	exit(98);
+
+return (m);
 }
