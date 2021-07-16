76cc821bde8c4eab94e7550db56234e3e63cd7a0
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-16 21:37:28 +0300

    Update holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,12 +1,5 @@
-#ifndef HOLBERTON
-#define HOLBERTON
-
-/**
- * File: holberton.h
- * Auth: Henok Gashaw
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
- */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -14,5 +7,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
