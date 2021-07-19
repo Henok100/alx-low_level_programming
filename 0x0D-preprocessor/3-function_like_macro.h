f5274477b6812cffe50c87e9b53614234994b580
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-19 19:32:56 +0300

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO
 #define FUNCTION_LIKE_MACRO
-
-/*
- * File: 3-function_like_macro.h
- * Auth: Henok_Gashaw
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
-
 #define ABS(x) (((x) < 0) ? (x * -1) : (x))
-
 #endif
