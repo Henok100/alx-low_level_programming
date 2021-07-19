cbadb09a481a98c3fd9a22fce81002aefadb4d91
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-19 11:06:28 +0300

    Create 1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI
+#define PI
+
+/*
+ * File: 1-pi.h
+ * Auth: Henok_Gashaw
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
