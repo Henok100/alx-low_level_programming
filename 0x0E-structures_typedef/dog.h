f17cd01a7be9623fdfb34f57f13d618dca226921
Henok100 <henokgashaw15@gmail.com>
2021-07-19 05:10:39 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,18 @@
 #ifndef DOG
 #define DOG
 
-/*
- * File: dog.h
- * Auth: Henok_Gashaw
- * Desc: Header file that defines a new type struct dog.
- */
-
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
 
 #endif
