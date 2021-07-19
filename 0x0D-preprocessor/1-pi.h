4712b1ffa6e0661430cd020e19feab176afdaedc
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-19 19:33:45 +0300

    Update 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,13 +1,3 @@
 #ifndef PI
-#define PI
-
-/*
- * File: 1-pi.h
- * Auth: Henok_Gashaw
- * Desc: Header file that defines a macro named PI
- *       as an abbreviation for the token 3.14159265359.
- */
-
 #define PI 3.14159265359
-
 #endif
