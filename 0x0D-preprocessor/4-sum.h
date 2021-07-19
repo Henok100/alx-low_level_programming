981018d337c4c46aeb14e65540b08afdac58a44c
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-19 11:11:24 +0300

    Create 4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM
+#define SUM
+
+/*
+ * File: 4-sum.h
+ * Auth: Henok_Gashaw
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
