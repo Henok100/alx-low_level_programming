b6f1a05127a17986b37decd100b16bcec453da50
Henok100 <65940650+Henok100@users.noreply.github.com>
2021-07-20 23:44:31 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef FUNCTION_POINTER
 #define FUNCTION_POINTER
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
