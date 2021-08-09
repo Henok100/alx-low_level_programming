4adc9cbb570a85a56befb8adcf391548aa089c6c
Henok Gashaw <65940650+Henok100@users.noreply.github.com>
2021-08-09 12:46:54 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN
+#define MAIN
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char);
@@ -8,4 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* MAIN */
