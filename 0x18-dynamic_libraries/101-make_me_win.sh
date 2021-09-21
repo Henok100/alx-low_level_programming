f054a4425772464ec81fe165a19060085e9503d7
Henok Gashaw <65940650+Henok100@users.noreply.github.com>
2021-09-21 10:06:00 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Henok100/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
